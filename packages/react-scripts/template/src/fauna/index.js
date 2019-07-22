6c46ac368d322cf10c38d4104b84bf62edfe682c
Nick Foden <nickfoden@gmail.com>
2019-07-22 19:59:03 -0400

    Change classes to collections

--- packages/react-scripts/template/src/fauna/index.js
+++ packages/react-scripts/template/src/fauna/index.js
@@ -1,5 +1,7 @@
 import faunadb, { query as q } from 'faunadb';
 
+//Now Updated for use with faunaDB 2.7.0
+//
 //First ! ! install faunadb either
 //yarn add faunadb
 //or
@@ -14,31 +16,33 @@ import faunadb, { query as q } from 'faunadb';
 const client = new faunadb.Client({ secret: process.env.REACT_APP_FAUNA_KEY });
 
 //And here are a few general functions written out as examples to get started
-const addNewClass = async newClass => {
+const addNewCollection = async newCollection => {
   try {
-    const ret = await client.query(q.CreateClass({ name: newClass }));
+    const ret = await client.query(q.CreateCollection({ name: newCollection }));
     return ret;
   } catch (err) {
     return console.error(err);
   }
 };
 
-//In addSingleRecord here below we are passing the relevant className and then the item as an object and assigning it the name of data in an effort to make this function reusable **This leaves it up to us to pass the same object shape or the shapes of our items will diverge
+//In addSingleRecord here below we are passing the relevant collectionName and then the item as an object and assigning it the name of data in an effort to make this function reusable **This leaves it up to us to pass the same object shape or the shapes of our items will diverge
 //Writing multiple "add" functions like addUser addBook addWidget and having more explicit shape such as { data: { title: "" author:""}} can help you enforce a schema / uniform shape for your items
 //https://docs.fauna.com/fauna/current/howto/crud#creating-a-post
-const addSingleRecord = async (className, data) => {
+const addSingleRecord = async (collectionName, data) => {
   try {
-    const ret = await client.query(q.Create(q.Class(className), { data }));
+    const ret = await client.query(
+      q.Create(q.Collection(collectionName), { data })
+    );
     return ret;
   } catch (err) {
     return console.log(err);
   }
 };
 
-const deleteSingleRecordByRef = async (className, itemRef) => {
+const deleteSingleRecordByRef = async (collectionName, itemRef) => {
   try {
     const ret = await client.query(
-      q.Delete(q.Ref(q.Class(className), itemRef))
+      q.Delete(q.Ref(q.Collection(collectionName), itemRef))
     );
     console.log('Deleted: ' + itemRef);
     return ret;
@@ -46,9 +50,11 @@ const deleteSingleRecordByRef = async (className, itemRef) => {
     return console.log(err);
   }
 };
-const getSingleRecordByRef = async (className, itemRef) => {
+const getSingleRecordByRef = async (collectionName, itemRef) => {
   try {
-    const ret = await client.query(q.Get(q.Ref(q.Class(className), itemRef)));
+    const ret = await client.query(
+      q.Get(q.Ref(q.Collection(collectionName), itemRef))
+    );
     return ret;
   } catch (err) {
     return console.log(err);
@@ -56,7 +62,7 @@ const getSingleRecordByRef = async (className, itemRef) => {
 };
 
 export {
-  addNewClass,
+  addNewCollection,
   addSingleRecord,
   deleteSingleRecordByRef,
   getSingleRecordByRef,
